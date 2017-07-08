a70dc70b24c51dd806188ea9ee3e1e043072a9a0
bananafish2828 <bananafish2828@gmail.com>
2017-07-07 22:46:01 -0700

    (add) friendController and endpoints

--- server/controller/friendController.js
+++ server/controller/friendController.js
@@ -6,7 +6,6 @@ const {
 const friendController = {
 
   getAllFriends: (req, res) => {
-    console.log('*** get all friends ***');
     Friend.findAll({ where : {
       userId: req.params.userId
     }})
@@ -17,31 +16,35 @@ const friendController = {
   },
 
   getFriendEntry: (req, res) => {
-    console.log('get friend entry');
-  },
-
-  updateFriendEntry: (req, res) => {
-    console.log('update friend entry');
-    Friend.findOne({ where: {
+    Friend.findAll({ where: {
       userId: req.params.userId,
       id: req.params.id
     }})
     .then((data) => {
+      res.status(200);
+      res.json(data);
+    })
+  },
+
+  updateFriendEntry: (req, res) => {
+    Friend.findOne({ where: {
+      userId: req.params.userId,
+      id: req.query.id
+    }})
+    .then((data) => {
       if (data) {
-        console.log('i exist - so update');
         Friend.update({
           name: req.body.name,
           phone: req.body.phone
         }, { where: {
           userId: req.params.userId,
-          id: req.params.id
+          id: req.query.id
         }})
         .then(() => {
           console.log('update successful');
           res.sendStatus(200);
         })
       } else {
-        console.log('i do not exist - so create');
         Friend.create({
           name: req.body.name,
           phone: req.body.phone,
@@ -61,6 +64,17 @@ const friendController = {
 
   deleteFriendEntry: (req, res) => {
     console.log('delete friend entry');
+    Friend.destroy({ where: {
+      userId: req.params.userId,
+      id: req.params.id
+    }})
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log('error deleting friend entry', err);
+      res.sendStatus(404);
+    })
   }
 
 }
