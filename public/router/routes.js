e6dd64f5eea6772787a27e6b9f485973fbce3584
Kay K <ds.kay.kim@gmail.com>
2017-07-10 14:40:42 -0700

    (update) fix logging in and out

--- public/router/routes.js
+++ public/router/routes.js
@@ -14,7 +14,31 @@ const handleAuthentication = (nextState, replace) => {
   }
 }
 
+
+
 export const makeMainRoutes = () => {
+  // let userId = null;
+  // if (auth.isAuthenticated()) {
+  //   // sets the localStorage to the user sub
+  //   // so we can post the user sub to the database
+  //   // if they are not in the database
+  //   auth.getUserInfo().client.userInfo(localStorage.access_token, function (err, user) {
+  //     // sets the userId 
+  //     userId = user.sub
+  //     postNewUser(user.sub)
+  //       .then((res) => {
+  //         getOneUser(user.sub)
+  //           .then((res) => {
+  //             localStorage.setItem('userId', res.data[0].id)
+  //             localStorage.setItem('userSub', res.data[0].idToken)
+  //           })
+  //           .catch(err => console.log(err))
+  //       })
+  //       .catch(err => console.log(err))
+  //   })
+  // }
+
+
   return (
       <BrowserRouter history={history} component={App}>
         <div>
@@ -22,9 +46,11 @@ export const makeMainRoutes = () => {
           <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
-            return <Callback {...props} /> 
+            return <Home auth={auth} userId={localStorage.userId} {...props} />
           }}/>
         </div>
       </BrowserRouter>
   );
 }
+
+            // <Home auth={auth} {...props} /> 
