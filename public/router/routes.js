b949c04b785879e32dfdb371e6180c871478a977
Kay K <ds.kay.kim@gmail.com>
2017-07-12 10:43:09 -0700

    (add) profile button to nav bar, (add) nav bar

--- public/router/routes.js
+++ public/router/routes.js
@@ -4,7 +4,9 @@ import App from '../components/app';
 import Home from '../Auth/Home/Home';
 import Callback from '../Auth/Callback/Callback';
 import Auth from '../Auth/Auth';
+import Profile from '../components/Profile/Profile.jsx'
 import history from '../Auth/history';
+import DejaBrewNavBar from '../components/Navbar/DejaBrewNavBar.jsx'
 
 const auth = new Auth();
 
@@ -14,44 +16,19 @@ const handleAuthentication = (nextState, replace) => {
   }
 }
 
-
-
 export const makeMainRoutes = () => {
-  // let userId = null;
-  // if (auth.isAuthenticated()) {
-  //   // sets the localStorage to the user sub
-  //   // so we can post the user sub to the database
-  //   // if they are not in the database
-  //   auth.getUserInfo().client.userInfo(localStorage.access_token, function (err, user) {
-  //     // sets the userId 
-  //     userId = user.sub
-  //     postNewUser(user.sub)
-  //       .then((res) => {
-  //         getOneUser(user.sub)
-  //           .then((res) => {
-  //             localStorage.setItem('userId', res.data[0].id)
-  //             localStorage.setItem('userSub', res.data[0].idToken)
-  //           })
-  //           .catch(err => console.log(err))
-  //       })
-  //       .catch(err => console.log(err))
-  //   })
-  // }
-
-
   return (
       <BrowserRouter history={history} component={App}>
         <div>
-          <Route path="/" render={(props) => <App auth={auth} {...props} />} />
-          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
-          {/*<Route path="/breweriesByBeerName" render={(props) => <breweriesByBeerName auth={auth} {...props} />} />*/}
+          <Route path='/' render={(props) => <DejaBrewNavBar auth={auth} {...props} />} />
+          <Route path="/home" render={(props) => <App auth={auth} {...props} />} />
+          <Route path="/profile" render={(props) => <Profile auth={auth} {...props} />} />
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
-            return <Home auth={auth} userId={localStorage.userId} {...props} />
+            return <App auth={auth} userId={localStorage.userId} {...props} />
           }}/>
+
         </div>
       </BrowserRouter>
   );
 }
-
-            // <Home auth={auth} {...props} /> 
