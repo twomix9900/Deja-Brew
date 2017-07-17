cdcc4ea65cc02ba483e2da11d270f62503e99549
Kay K <ds.kay.kim@gmail.com>
2017-07-17 16:14:48 -0700

    (add) redux link to details page

--- public/components/Details/Details.jsx
+++ public/components/Details/Details.jsx
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Search from '../../components/Search/search.jsx'
+import Search from '../../components/Search/search.jsx';
+import actions from '../../actions';
+import { connect, Store } from 'react-redux';
 
 class Details extends React.Component {
   constructor(props) {
     // console.log('props from Details! = ', props)
     super(props);
   }
+  
+  componentDidMount() {
+
+  }
 
   render() {
     console.log('this.props = ', this.props);
     return (
       <div>
-        DETAILS PAGE SCAFFOLDING
+        <h1>BEER NAME: {this.props.venue.selectedVenue.breweries[0].name}</h1>
+        BREWERY ICON: {this.props.venue.selectedVenue.breweries[0].images ? <img src={this.props.venue.selectedVenue.breweries[0].images.squareMedium} alt="boohoo" className="img-responsive" /> : null}
+        <h3>BREWERY ID: {this.props.venue.selectedVenue.breweries[0].id}</h3> 
       </div>
-      // <div>
-      //     <MuiThemeProvider>
-      //       <Search
-      //         handleBreweriesByBeerNameSearch={this.handleBreweriesByBeerNameSearch.bind(this)}
-      //         handleBreweriesByBreweryNameSearch={this.handleBreweriesByBreweryNameSearch.bind(this)}
-      //         handleBreweriesByLocationSearch={this.handleBreweriesByLocationSearch.bind(this)}
-      //       />
-      //     </MuiThemeProvider>
-      // </div>
     );
   }
 }
+  
 
-export default Details;
+const stateToProps = (state) => {
+  return {
+    venue: state.venue
+  }
+}
+
+const dispatchToProps = (dispatch) => {
+  return {
+    selectVenue: (venue) => {
+      dispatch(actions.selectVenue(venue));
+    }
+  }
+}
+
+export default connect(stateToProps, dispatchToProps)(Details);
