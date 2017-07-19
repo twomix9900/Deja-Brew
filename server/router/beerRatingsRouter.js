45a752a93addd8f0534478dc203fff90bff32022
bananafish2828 <bananafish2828@gmail.com>
2017-07-19 08:39:36 -0700

    (fix) ratings db endpoints

--- server/router/beerRatingsRouter.js
+++ server/router/beerRatingsRouter.js
@@ -3,7 +3,7 @@ const beerRatingsRouter = express.Router();
 
 const beerRatingsController = require('../controller/beerRatingsController.js');
 
-beerRatingsRouter.get('/:id/:beerId', beerRatingsController.getBeerRating);
+beerRatingsRouter.get('/:beerId', beerRatingsController.getBeerRating);
 beerRatingsRouter.put('/:id', beerRatingsController.updateBeerRating);
 
 module.exports = beerRatingsRouter;
--- server/router/breweryRatingsRouter.js
+++ server/router/breweryRatingsRouter.js
@@ -3,7 +3,7 @@ const breweryRatingsRouter = express.Router();
 
 const breweryRatingsController = require('../controller/breweryRatingsController.js');
 
-breweryRatingsRouter.get('/:id/:breweryId', breweryRatingsController.getBreweryRating);
+breweryRatingsRouter.get('/:breweryId', breweryRatingsController.getBreweryRating);
 breweryRatingsRouter.put('/:id', breweryRatingsController.updateBreweryRating);
 
 module.exports = breweryRatingsRouter;
