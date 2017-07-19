17358eadb97630f54ff729a5f9acfbb60f066810
Adewale Balogun <abalogun93@gmail.com>
2017-07-18 20:03:44 -0700

    (UPDATE)adds changes for details

--- public/components/Details/Details.jsx
+++ public/components/Details/Details.jsx
@@ -24,12 +24,12 @@ class Details extends React.Component {
         <MuiThemeProvider>
           <div>
             <Paper style={style} zDepth={5}>
-              <h1>BEER NAME: {this.props.venue.selectedVenue.breweries[0].name}</h1>
-              BREWERY ICON: {this.props.venue.selectedVenue.breweries[0].images ? <img src={this.props.venue.selectedVenue.breweries[0].images.squareMedium} alt="boohoo" className="img-responsive" /> : null}
+              <h1>BEER NAME: {this.props.venue.selectedVenue.name}</h1>
+              BREWERY ICON: {this.props.venue.selectedVenue.brewery.images ? <img src={this.props.venue.selectedVenue.brewery.images.large} alt="boohoo" className="img-responsive" /> : null}
               <h3>ABV: {this.props.venue.selectedVenue.abv}</h3>
-              <h3>BREWERY ID: {this.props.venue.selectedVenue.breweries[0].id}</h3>
-              <h3>WEBSITE: {this.props.venue.selectedVenue.breweries[0].website}</h3>
-              <p>{this.props.venue.selectedVenue.style.description}</p>
+              <h3>BREWERY ID: {this.props.venue.selectedVenue.breweryId}</h3>
+              <h3>WEBSITE: {this.props.venue.selectedVenue.brewery.website}</h3>
+              <p>{this.props.venue.selectedVenue.brewery.description}</p>
             </Paper>
           </div>
         </MuiThemeProvider> 
