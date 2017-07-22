173702ebfe40147bb22b1e8c46e48109c0be96b1
bananafish2828 <bananafish2828@gmail.com>
2017-07-21 19:26:05 -0700

    (refactor) email

--- public/components/Profile/Email/Email.jsx
+++ public/components/Profile/Email/Email.jsx
@@ -7,18 +7,23 @@ export default class Email extends Component {
 
   constructor(props) {
     super(props);
-    this.state={}
+    this.state={
+      displayEmail: true,
+      email: ''
+    }
     this.editEmail=this.handleEditEmail.bind(this);
     this.submitEmail=this.handleSubmitEmail.bind(this);
   }
 
   componentWillReceiveProps(NextProps) {
-    this.setState({ email: NextProps.email })
-    this.setState({ DisplayEmail: <DisplayEmail handleEmailClick={ this.editEmail } email={ NextProps.email } /> });
+    // this.setState({ email: NextProps.email })
+    // this.setState({ DisplayEmail: <DisplayEmail handleEmailClick={ this.editEmail } email={ NextProps.email } /> });
+    this.setState({ displayEmail: true, email: NextProps.email });
   }
 
   handleEditEmail() {
-    this.setState({ DisplayEmail: <QueryEmail handleSubmit={ this.submitEmail } /> });
+    // this.setState({ DisplayEmail: <QueryEmail handleSubmit={ this.submitEmail } /> });
+    this.setState({ displayEmail: false });
   }
 
   handleSubmitEmail(emailSubmission) {
@@ -26,17 +31,24 @@ export default class Email extends Component {
       this.setState({ email: emailSubmission });
       axios.put('/users/' + this.props.userId, { email: emailSubmission })
       .then(() => {
-        this.setState({ DisplayEmail: <DisplayEmail handleEmailClick={ this.editEmail } email={ emailSubmission } /> });
+        // this.setState({ DisplayEmail: <DisplayEmail handleEmailClick={ this.editEmail } email={ emailSubmission } /> });
+        this.setState({ displayEmail: true, email: emailSubmission });
       })
     } else {
-      this.setState({ DisplayEmail: <DisplayEmail handleEmailClick={ this.editEmail } email={ this.state.email } /> });
+      // this.setState({ DisplayEmail: <DisplayEmail handleEmailClick={ this.editEmail } email={ this.state.email } /> });
+      this.setState({ displayEmail: true, email: this.state.email });
     }
   }
 
   render() {
     return (
       <div>
-        { this.state.DisplayEmail }
+        {/* { this.state.DisplayEmail } */}
+        ({ this.state.displayEmail }) ? (
+          <DisplayEmail handleEmailClick={ this.editEmail } email={ this.state.email } />
+        ) : (
+          <QueryEmail handleSubmit={ this.submitEmail } />
+        )
       </div>
     )
   }
