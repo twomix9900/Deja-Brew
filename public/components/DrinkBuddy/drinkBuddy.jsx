7b72241aa74ba714d41a1b37bffc01e310da1883
bananafish2828 <bananafish2828@gmail.com>
2017-07-23 08:14:24 -0700

    (fix) checkbox clickaway

--- public/components/DrinkBuddy/drinkBuddy.jsx
+++ public/components/DrinkBuddy/drinkBuddy.jsx
@@ -20,10 +20,14 @@ export default class drinkBuddy extends Component {
     super(props);
     this.state={
       friendList: [],
-      newFriendQuery: true
+      newFriendQuery: true,
+      selected: [],
+      selectedCache: []
     }
     this.handleFriendAdd=this.handleFriendAdd.bind(this);
     this.handleSubmit=this.handleSubmit.bind(this);
+    this.handleRowSelection=this.handleRowSelection.bind(this);
+    this.handleFriendSelect=this.handleFriendSelect.bind(this);
     this.sortFriendList=this.sortFriendList.bind(this);
     this.formatPhone=this.formatPhone.bind(this);
   }
@@ -51,24 +55,19 @@ export default class drinkBuddy extends Component {
   this.setState({ friendList: sortedFriendList })
   }
 
-  getFriendData() {
-    let userInfo=JSON.parse(localStorage.getItem('userInfo'));
-    axios.get('/friends/' + userInfo.id)
-    .then((data) => {
-      return data.data;
-    })
-    .then((data) => {
-      this.setState({ friendList: data });
-      this.sortFriendList();
-    })
-  }  
-  
   formatPhone(phone) {
     return '(' + phone.substring(3, 6) + ') ' + phone.substring(6, 9) + '-' + phone.substring(9);
   }
 
+  isSelected(idx) {
+    return this.state.selected.indexOf(idx) !== -1;
+  }
+
   handleFriendAdd() {
+    console.log('selected', this.state.selected, this.state.selectedCache);
     this.setState({ newFriendQuery: false })
+    console.log('*** selected', this.state.selected, this.state.selectedCache);
+
   }
 
   handleSubmit(friendName, phone) {
@@ -76,8 +75,8 @@ export default class drinkBuddy extends Component {
       this.setState({ newFriendQuery: true })
     } else {
       let userInfo=JSON.parse(localStorage.getItem('userInfo'));
-      let list=this.state.friendList;
-      list.push({ name: friendName, phone: '+1 ' + phone });
+      let list = this.state.friendList;
+      list.push({ name: friendName, phone: '+1 ' + phone, selected: true });
       this.setState({ friendList: list })
       axios.put('friends/' + userInfo.id, { name: friendName, phone: '+1 ' + phone })
       .then(() => {
@@ -87,19 +86,51 @@ export default class drinkBuddy extends Component {
     }
   }
 
+  handleRowSelection(selectedRows) {
+    console.log('*** what is this', selectedRows, this.state.selected)
+    if (selectedRows == []) {
+      this.setState({ selectedCache: this.state.selected })
+    }
+    this.setState({ selected: selectedRows });
+    // let list = this.state.friendList;
+    // if (selectedRows === 'all') {
+    //   for (let i = 0; i < list.length; i++) {
+    //     list[i].selected = true;
+    //   }
+    // } else if (selectedRows === 'none') {
+    //   for (let i = 0; i < list.length; i++) {
+    //     list[i].selected = false;
+    //   }
+    // } else {
+    //   for (let i = 0; i < selectedRows.length; i++) {
+    //     list[selectedRows[i]].selected = true;
+    //   }
+    // }
+    // this.setState({ friendList: list })
+  }
+
+  handleFriendSelect(i) {
+    let list = this.state.friendList;
+    list[i].selected = !list[i].selected;
+    this.setState({ friendList: list });
+  }
+
   render() {
     return (
       <div><AppBar title="my drinking buddies" showMenuIconButton={false} />
-        <Table multiSelectable={true}>
+        <Table multiSelectable={true} onRowSelection={ this.handleRowSelection }>
           <TableHeader enableSelectAll={true}>
             <TableRow>
               <TableHeaderColumn>Name</TableHeaderColumn>
               <TableHeaderColumn>Phone Number</TableHeaderColumn>
             </TableRow>
           </TableHeader>
-          <TableBody>
+          <TableBody deselectOnClickaway={ false }>
             { this.state.friendList.map((friend, i) =>
-            <TableRow key = { i }>
+            <TableRow 
+              key={ i } 
+              selected={ this.isSelected(i) } 
+            >
               <TableRowColumn>{ friend.name }</TableRowColumn>
               <TableRowColumn>{ this.formatPhone(friend.phone) }</TableRowColumn>
             </TableRow>
