327a3a13869a28d521b8e5db89bca85ab4e25f3c
Adewale Balogun <abalogun93@gmail.com>
2017-07-24 20:31:35 -0700

    (UPDATE)git status makes breweryinfo into a function to render

--- public/components/Details/Details.jsx
+++ public/components/Details/Details.jsx
@@ -7,9 +7,8 @@ import { connect, Store } from 'react-redux';
 import Paper from 'material-ui/Paper';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import RaisedButton from 'material-ui/RaisedButton';
-
-
 import DrinkBuddy from '../DrinkBuddy/drinkBuddy.jsx'
+
 
 class Details extends React.Component {
   constructor(props) {
@@ -17,22 +16,23 @@ class Details extends React.Component {
     console.log('props from Details! = ', props)
     this.state = {
       userInfo: {},
-      beersFromBrewery: [],
-      pageOfItems: []
+      beersFromBrewery: []
     }
     this.sendDirections = this.sendDirections.bind(this);
     this.getBeersFromBrewery = this.getBeersFromBrewery.bind(this);
     this.renderBeerList = this.renderBeerList.bind(this);
+    this.renderBrewInfo = this.renderBrewInfo.bind(this);
   }
   
   componentWillMount() {
     console.log(this.state.beersFromBrewery)
     let info = JSON.parse(localStorage.getItem('userInfo'));
     this.setState({ userInfo: info });
-    console.log('props in details beers brew-->',this.props.venue.selectedVenue.id);
+    let brewInfo = this.props.venue.selectedVenue;
+    console.log('props in details beers brew-->', brewInfo);
     this.getBeersFromBrewery();
   }
-
+ 
   getBeersFromBrewery() {
     axios.get('/brewery/beers/' + this.props.venue.selectedVenue.id)
     .then((data) => {
@@ -41,6 +41,33 @@ class Details extends React.Component {
       })
     })
     .catch((err) => console.log('getBeersFromBrewery err = ', err))
+  }
+
+  renderBrewInfo() {
+    let brewInfo = this.props.venue.selectedVenue;
+      return (
+        <div>
+          <h1>Name: {brewInfo.brewery ? brewInfo.brewery.name : brewInfo.name}</h1>
+          <h5>Icon: 
+            {/*{(brewInfo.brewery.images ? brewInfo.brewery.images.large : 'no pic') || (brewInfo.images ? brewInfo.images.large : 'no pic')} */}
+            {brewInfo.images ? <img src={brewInfo.images.squareMedium} alt="boohoo" className="img-responsive" /> : null || brewInfo.brewery.images.squareMedium}
+          </h5>
+          <h3>
+            Hours: {brewInfo.hoursOfOperation ? brewInfo.hoursOfOperation : 'no hours'}
+          </h3>
+          <h4>
+            BreweryId: {(brewInfo.breweryId ? brewInfo.breweryId : brewInfo.id) || (brewInfo.id ? brewInfo.id : 'no id')}
+          </h4>
+          <h4>
+            Website:{/*{(brewInfo.brewery.website ? brewInfo.brewery.website : brewInfo.website) || (brewInfo.website ? brewInfo.website : 'no website')}*/}
+            {brewInfo.website || brewInfo.brewery.website}
+          </h4>
+          <p>
+            Description:{/*{(brewInfo.brewery.description ? brewInfo.brewery.description : 'no description') || (brewInfo.description ? brewInfo.description : 'no description')}*/}
+            {brewInfo.description || brewInfo.brewery.description}
+          </p>
+        </div>
+      )
   }
 
   renderBeerList() {
@@ -76,13 +103,8 @@ class Details extends React.Component {
           <div>
           
             <Paper style={style} zDepth={5}>
-              <h1>Brewery Name: {this.props.venue.selectedVenue.name || this.props.venue.selectedVenue.brewery.name}</h1>
-              Brewery Icon: {this.props.venue.selectedVenue.images ? <img src={this.props.venue.selectedVenue.images.large} alt="boohoo" className="img-responsive" /> : null || this.props.venue.selectedVenue.brewery.images.large}
-              <h3>Hours: {this.props.venue.selectedVenue.hoursOfOperation || this.props.venue.selectedVenue.locations.hoursOfOperation}</h3>
-              <h3>Brewery Id: {this.props.venue.selectedVenue.id || this.props.venue.selectedVenue.breweryId}</h3>
-              <h3>Website: {this.props.venue.selectedVenue.website || this.props.venue.selectedVenue.brewery.website}</h3>
-              <p>Description: {this.props.venue.selectedVenue.description || this.props.venue.selectedVenue.brewery.description}</p>
-              <h3>Beer List:</h3>
+              {this.renderBrewInfo()}
+              <h3>Beer List: </h3>
               <ul>
                {this.renderBeerList()}
               </ul> 
